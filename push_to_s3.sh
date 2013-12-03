a627913c26ec44c7e3ac1d7814048660442a884f
Gojko Adzic <gojko@gojko.com>
2013-12-03 21:14:59 +0000

    pulled from s3

--- push_to_s3.sh
+++ push_to_s3.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-aws s3 sync --acl public-read content s3://static.mindmup.com
+# run with --dryrun to see what will happen
+aws s3 sync $1 --acl public-read content s3://static.mindmup.com
