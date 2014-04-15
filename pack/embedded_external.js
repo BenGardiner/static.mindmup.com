73d546d850a200e78ec3cec57a2b34aba0560aa2
Gojko Adzic <gojko@gojko.com>
2014-04-15 01:21:47 +0100

    taking kinetic out of embedded scripts

--- pack/embedded_external.js
+++ pack/embedded_external.js
@@ -1,7 +1,5 @@
 content/lib/jquery-2.0.2.min.js
 content/lib/underscore-1.4.4.min.js
-content/lib/jquery.mousewheel-3.1.3.js
 content/lib/color-0.4.1.min.js
-content/lib/kinetic-v4.5.4.min.js
 content/lib/jquery.hammer.min.js
 content/lib/jquery.hotkeys.js
