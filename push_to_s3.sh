25b755d20885500f904f923c93dd5d96fb34021a
Gojko Adzic <gojko@gojko.com>
2014-04-28 13:47:29 +0100

    added url ping for fonts

--- push_to_s3.sh
+++ push_to_s3.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # run with --dryrun to see what will happen
 aws s3 sync $1 --acl public-read content s3://static.mindmup.com
+curl --verbose -H "Origin: http://www.mindmup.com" -H "Access-Control-Request-Method: GET" http://static.mindmup.com/font/3.2.1b/fontawesome-webfont.woff -o /dev/null
