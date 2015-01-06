8c27b623c4d2c01e4cc74919cf2fcf1e510f480d
Gojko Adzic <gojko@gojko.com>
2015-01-06 14:28:39 +0100

    jq upgrade

--- pack/embedded_external.js
+++ pack/embedded_external.js
@@ -1,4 +1,4 @@
-content/lib/jquery-2.0.2.min.js
+content/lib/jquery-2.1.3.min.js
 content/lib/underscore-1.4.4.min.js
 content/lib/color-0.4.1.min.js
 content/lib/jquery.hammer.min.js
--- pack/external.js
+++ pack/external.js
@@ -1,4 +1,4 @@
-content/lib/jquery-2.0.2.min.js
+content/lib/jquery-2.1.3.min.js
 content/lib/underscore-1.4.4.min.js
 content/lib/bootstrap-2.3.1.min.js
 content/lib/jquery-ui-1.10.0.custom.min.js
